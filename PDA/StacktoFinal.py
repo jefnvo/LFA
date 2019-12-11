f6f9d81435e6e4351340cd2b35e12c721614bf96
Jefferson Oliveira <jeffin.f21@gmail.com>
2019-12-11 01:05:22 -0300

    consertei esse infernooooo

---
+++ PDA/StacktoFinal.py
@@ -0,0 +1,152 @@
+def handleStack(stack):
+    global register
+
+    current = register.pop()
+
+    if current[1] == 'E':
+        stack.append(current[2])
+
+    elif len(current) >= 3:
+        for i in range(1,len(current)-1):
+            stack.pop()
+
+def getMoves(state,automata):#q0 - 0011
+    global index, flag, handler, register, stack
+
+    nextState = state
+    
+
+    for r in rules: #[q0 0 z0 [q0 0 z0 ]]    
+        if index < len(automata) or r[1] == 'E':
+            if r[1] == 'E':
+                if state == r[0]:
+                    if len(stack) > 0:
+                        if stack[len(stack)-1] == r[2]:                            
+                            topStack = r[3]                        
+                            print("E - state change of " + str(state) + " to " + str(topStack[0]) + "\n")
+                            if topStack[1] == 'E':
+                                stack.pop()
+                                topStack.append(r[2])
+                                register.append(topStack)
+                            else:
+                                if len(topStack) >= 3:
+                                    for i in range(1,len(topStack)-1):
+                                        stack.append(topStack[i])                                    
+                                register.append(topStack)
+                            print("stack: " + str(stack) + "\n\n")
+                            nextState = topStack[0]
+                            if(state != 'p0'):
+                                index = index + 1
+                            getMoves(nextState,automata)
+                            handleStack(stack)
+                            print("Rollback to state " + str(state) + " with stack " + str(stack) + "\n\n")
+            elif automata[index] == r[1]: #[q0 0 z0 [q0 0 z0]]                
+                if state == r[0]:
+                    if len(stack) > 0:
+                        if stack[len(stack)-1] == r[2]: 
+                            topStack = r[3] #[q0 0 z0]                            
+                            print(str(automata[index]) + " - state change from " + str(state) + " to " + str(topStack[0]) + "\n")                            
+                            if topStack[1] == 'E':
+                                stack.pop()
+                            elif len(topStack) >= 3:
+                                    #get the word and push in stack                                    
+                                    for i in range(1,len(topStack)-1):
+                                        stack.append(topStack[i])
+                            if topStack[1] == 'E' and len(stack) > 0:
+                                topStack.append(r[2])
+                                register.append(topStack)
+                            else:
+                                register.append(topStack)
+                            print("stack: " + str(stack) + "\n\n")                            
+                            nextState = topStack[0]
+                            index = index + 1                            
+                            getMoves(nextState,automata)
+                            index = index - 1                            
+                            handleStack(stack)
+
+                            print("Rollback to state " + str(state) + " with stack " + str(stack) + "\n\n")
+        elif len(stack) == 0:
+            flag = 1
+            print("------ACCEPTED HERE!------\n\n")            
+            return
+
+def main(automata, defAutomata):
+    global handler, index, flag, rules, register, stack
+    ##The next lines will be populate our automata from the definition file##
+    handler = open(defAutomata, "r")
+    lines = handler.readlines()
+    handler.close()
+
+    states = lines[0].split()    
+    #get the alphabet
+    alphabet = []
+    for word in lines[1]:
+        if word != '\n':
+            alphabet.append(word)
+
+    stack_symbol = lines[2].split()
+    stack = ['x0']
+
+    initial_state = 'p0'
+
+    #get the rules
+    rules = []
+    n_rules = len(lines) - 4
+    for i in range(4,len(lines)):
+        rules.append(lines[i].split())
+    
+    for i in range(0,n_rules):
+        aux = rules[i] # q0 0 z0 q0 0,z0 | q1 0 1 q1 E
+        aux2 = aux[4].split(",") #[0 z0] | [E]
+        aux2.insert(0,aux[3]) # [q0 0 z0]| [q1, E]
+        del(aux[4]) # q0 0 z0 q0         | q1 0 1 q1 
+        del(aux[3]) # q0 0 z0            | 
+        aux.append(aux2) # [q0 0 z0 [q0 0 z0 ]] | [q1 0 1 [q1, E]]
+        rules[i] = aux #[q0 0 z0 [q0 0 z0 ]]
+
+    ## adding wrappers ##
+    for s in states:
+        newRule = [str(s), 'E', 'x0', ['p', 'E']]
+        rules.append(newRule)
+        n_rules = n_rules + 1
+    states.append('p')
+    states.append('p0')
+    newRuleStart = ['p0','E', stack[0], [lines[3].split()[0], stack_symbol[0], 'x0',]]
+    rules.append(newRuleStart)
+    n_rules = n_rules + 1
+    for i in range(0,n_rules):
+        print(rules[i])
+
+
+    
+    
+    
+    
+    print("Alphabet: " + str(alphabet) + "\n" + 
+          "States: " + str(states) + "\n" + 
+          "Initial State: " + str(initial_state) + "\n" + 
+          "Stack symbol: " + str(stack[0]))
+
+    print("\n----------LEVELS---------\n\n")
+    print("automata at level: " + automata + "\n")
+
+    print('\n')
+
+    index = 0
+    flag = 0
+
+    register = []
+    getMoves(initial_state,automata)
+
+    if flag == 0:
+        print(automata+ " -> The tape was reject!")
+    elif flag == 1:
+        print(automata+ " -> The tape was accept!")
+
+
+
+main('E10',"./automata_empty_definition.txt")
+
+
+
+
